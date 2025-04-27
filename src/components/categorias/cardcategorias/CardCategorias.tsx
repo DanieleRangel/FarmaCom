a9e11f8c64b8a685ecf6c850222f9b9399dd46bc
Daniele Rangel <danielecrangel@outlook.com>
2025-04-27 20:53:18 -0300

    ajuste cardcategoria links

--- src/components/categorias/cardcategorias/CardCategorias.tsx
+++ src/components/categorias/cardcategorias/CardCategorias.tsx
@@ -5,7 +5,7 @@ interface CardCategoriasProps {
     categoria: Categoria
 }
 
-function CardCategorias({ }: CardCategoriasProps) {
+function CardCategorias({categoria }: CardCategoriasProps) {
     return (
         <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
             <header className='py-2 px-6 bg-indigo-800 text-white font-bold text-2xl'>
@@ -14,13 +14,13 @@ function CardCategorias({ }: CardCategoriasProps) {
             <p className='p-8 text-3xl bg-slate-200 h-full'>Descrição</p>
 
             <div className="flex">
-                <Link to='' 
+                <Link to={`/editarcategoria/${categoria.id}`} 
                     className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 
                         flex items-center justify-center py-2'>
                     <button>Editar</button>
                 </Link>
 
-                <Link to='' className='text-slate-100 bg-red-400 hover:bg-red-700 w-full 
+                <Link to={`/deletarcategoria/${categoria.id}`}  className='text-slate-100 bg-red-400 hover:bg-red-700 w-full 
                     flex items-center justify-center'>
                     <button>Deletar</button>
                 </Link>
