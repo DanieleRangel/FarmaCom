bdcbf5ad2a871d2dd0c580c9a26d71a7475af097
Daniele Rangel <danielecrangel@outlook.com>
2025-04-28 15:49:38 -0300

    ajustes estilização

--- src/components/categorias/formcategoria/FormCategoria.tsx
+++ src/components/categorias/formcategoria/FormCategoria.tsx
@@ -71,11 +71,11 @@ function FormCategoria() {
     return (
     <div className="flex justify-center w-full items-center py-25 h-211">
         <div className="container flex justify-center box-content">
-            <div className=" border border-solid border-[#5E5C70] h-110 flex flex-col justify-evenly items-center text-2xl  text-[#F7F5F1] w-150 rounded-2xl bg-[#D8BFD8]">
+            <div className=" border border-solid border-b-black h-110 flex flex-col justify-evenly items-center text-2xl  text-white w-150 rounded-2xl bg-gray-400">
                 <h1 className="font-bold">{id === undefined ? 'Nova Categoria' : 'Alterar Categoria Existente'}</h1>
 
                 <form onSubmit={gerarNovaCategoria} className="flex flex-col h-90 justify-evenly">
-                    <div className="flex flex-col text-lg text-[#F7F5F1] font-medium gap-2">
+                    <div className="flex flex-col text-lg text-white font-medium gap-2">
                         <label htmlFor="nome">Nome</label>
                         <input  
                             type="text" 
@@ -83,10 +83,10 @@ function FormCategoria() {
                             name="nome"
                             value={categoria.nome}
                             onChange={(e:ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                            className="text-[#F7F5F1] border border-solid border-[#5E5C70] p-3 w-130 rounded-lg font-normal"
+                            className="text-white border border-solid border-b-black p-3 w-130 rounded-lg font-normal"
                         />
                     </div>
-                    <div className="flex flex-col text-lg text-[#F7F5F1] font-medium gap-2">
+                    <div className="flex flex-col text-lg text-white font-medium gap-2">
                         <label htmlFor="descricao">Descrição</label>
                         <input  
                             type="text" 
@@ -94,12 +94,12 @@ function FormCategoria() {
                             name="descricao" 
                             value={categoria.descricao}
                             onChange={(e:ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                            className="text-[#B98DB9] border border-solid border-[#5E5C70] p-3 w-130 rounded-lg font-normal"
+                            className="text-white border border-solid border-b-black p-3 w-130 rounded-lg font-normal"
                         />
                     </div>
                     <div className=" text-lg font-bold flex flex-row gap-4">
-                        <button type="reset" onClick={cancelar} className="border border-solid border-gray-300 w-50 text-[#A7A5B6]  bg-[#F7F5F1] p-2 rounded-lg cursor-pointer hover:shadow-xl">Cancelar</button>
-                        <button type="submit" className="bg-[#A7A5B6] w-76 text-[#F7F5F1] p-2 rounded-lg cursor-pointer hover:shadow-xl">
+                        <button type="reset" onClick={cancelar} className="border border-solid border-b-gray-100 w-50 text-white  bg-red-500 p-2 rounded-lg cursor-pointer hover:shadow-xl">Cancelar</button>
+                        <button type="submit" className="border border-solid bg-[#26b360] w-76 text-white p-2 rounded-lg cursor-pointer hover:shadow-xl">
                         {isLoading ?
                             <RotatingLines
                                 strokeColor="white"
@@ -124,4 +124,4 @@ function FormCategoria() {
     )
 }
 
-export default FormCategoria
+export default FormCategoria;
